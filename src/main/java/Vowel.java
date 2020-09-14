e10427bac74755492aae0cec70594c387b81e5eb
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-14 16:56:06 +0800

    [FuFangyuan] test null

---
+++ src/main/java/Vowel.java
@@ -0,0 +1,9 @@
+public class Vowel {
+
+
+    public void mommifier(String word) throws Exception {
+        if(word==null){
+            throw new Exception();
+        }
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MommifierTest {
+
+    @Test
+    void should_throw_exception_when_the_word_is_null(){
+        assertThrows(Exception.class,()->{
+            Vowel vowel = new Vowel();
+            vowel.mommifier(null);
+        });
+
+    }
 }
