d61886f467b2813e04582f63b0e75bdee573f250
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-14 17:25:28 +0800

    [FuFangyuan] test under 30%

--- src/main/java/Vowel.java
+++ src/main/java/Vowel.java
@@ -1,9 +1,31 @@
 public class Vowel {
 
 
-    public void mommifier(String word) throws Exception {
+    public String mommifier(String word) throws Exception {
         if(word==null){
             throw new Exception();
         }
+
+        if(isRateUnder30Percent(word)){
+            return word;
+        }
+
+        return null;
+    }
+
+    private Boolean isRateUnder30Percent(String word){
+        double count = 0;
+        for(int i = 0;i<word.length();i++){
+            char letter = word.charAt(i);
+            if(isVowels(letter)){
+                count++;
+            }
+        }
+        return count/word.length()<0.3;
+    }
+
+    private Boolean isVowels(char letter){
+        return letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'||
+                letter == 'A'|| letter == 'E'|| letter == 'I'|| letter == 'O'|| letter == 'U';
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MommifierTest {
@@ -10,6 +11,13 @@ public class MommifierTest {
             Vowel vowel = new Vowel();
             vowel.mommifier(null);
         });
+    }
 
+    @Test
+    void should_return_original_word_when_number_of_vowels_under_30_percent() throws Exception {
+        Vowel vowel = new Vowel();
+        String wordUnder30Percent = "test";
+        String result =vowel.mommifier(wordUnder30Percent);
+        assertEquals(wordUnder30Percent,result);
     }
 }
