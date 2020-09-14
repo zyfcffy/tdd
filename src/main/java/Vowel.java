4881c3392998895d891af2cd5b33562e24a5446f
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-14 17:59:49 +0800

    [FuFangyuan] modify the null Expection and finish rest

--- src/main/java/Vowel.java
+++ src/main/java/Vowel.java
@@ -1,31 +1,53 @@
 public class Vowel {
+    private static final String INSETWORD = "mommy";
+    private static final double RATE = 0.3;
 
 
-    public String mommifier(String word) throws Exception {
-        if(word==null){
-            throw new Exception();
+    public String mommifier(String word) {
+        int insertIndex = indexOfContinuousVowel(word);
+
+        if (word == null) {
+            throw new NullPointerException();
         }
 
-        if(isRateUnder30Percent(word)){
+        if (isRateUnder30Percent(word) || insertIndex == -1) {
             return word;
         }
 
-        return null;
+        StringBuilder wordBuilder = new StringBuilder(word);
+        StringBuilder result = wordBuilder.insert(insertIndex, INSETWORD);
+        return mommifier(result.toString());
     }
 
-    private Boolean isRateUnder30Percent(String word){
+
+    private Boolean isRateUnder30Percent(String word) {
         double count = 0;
-        for(int i = 0;i<word.length();i++){
+        for (int i = 0; i < word.length(); i++) {
             char letter = word.charAt(i);
-            if(isVowels(letter)){
+            if (isVowels(letter)) {
                 count++;
             }
         }
-        return count/word.length()<0.3;
+        return count / word.length() < RATE;
     }
 
-    private Boolean isVowels(char letter){
-        return letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'||
-                letter == 'A'|| letter == 'E'|| letter == 'I'|| letter == 'O'|| letter == 'U';
+    private Integer indexOfContinuousVowel(String word) {
+        int index = -1;
+        for (int i = 1; i < word.length(); i++) {
+            char letter = word.charAt(i);
+            if (isVowels(letter)) {
+                if (letter == word.charAt(i - 1)) {
+                    index = i;
+                }
+            }
+        }
+        return index;
     }
+
+    private Boolean isVowels(char letter) {
+        return letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
+                letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U';
+    }
+
+
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,23 +1,40 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MommifierTest {
 
     @Test
-    void should_throw_exception_when_the_word_is_null(){
-        assertThrows(Exception.class,()->{
+    void should_return_null_when_the_word_is_null() {
+        assertThrows(NullPointerException.class, ()-> {
             Vowel vowel = new Vowel();
             vowel.mommifier(null);
         });
     }
 
     @Test
-    void should_return_original_word_when_number_of_vowels_under_30_percent() throws Exception {
+    void should_return_original_word_when_number_of_vowels_under_30_percent() {
         Vowel vowel = new Vowel();
         String wordUnder30Percent = "test";
-        String result =vowel.mommifier(wordUnder30Percent);
-        assertEquals(wordUnder30Percent,result);
+        String result = vowel.mommifier(wordUnder30Percent);
+        assertEquals(wordUnder30Percent, result);
     }
+
+    @Test
+    void should_return_original_word_when_vowels_is_not_continuous(){
+        Vowel vowel = new Vowel();
+        String wordNotContinuous = "owe";
+        String result = vowel.mommifier(wordNotContinuous);
+        assertEquals(wordNotContinuous,result);
+    }
+
+    @Test
+    void should_return_mommify_word(){
+        Vowel vowel = new Vowel();
+        String wordCanBeMommified = "kemommyemommyep";
+        String result = vowel.mommifier(wordCanBeMommified);
+        assertEquals(wordCanBeMommified,result);
+    }
+
 }
